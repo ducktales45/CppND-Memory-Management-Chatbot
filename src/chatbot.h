9aaf29519867d94ade06ded5f21f964a69afcb12
root <meaganalbright4@gmail.com>
2020-06-24 18:33:29 +0000

    changes for task 2

--- src/chatbot.h
+++ src/chatbot.h
@@ -3,6 +3,7 @@
 
 #include <wx/bitmap.h>
 #include <string>
+#include <iostream>
 
 class GraphNode; // forward declaration
 class ChatLogic; // forward declaration
@@ -26,12 +27,68 @@ public:
     ChatBot();                     // constructor WITHOUT memory allocation
     ChatBot(std::string filename); // constructor WITH memory allocation
     ~ChatBot();
+    // Task 2: Implement Rule of 5 for chatbot object.
+    // copy constructor
+    ChatBot (const ChatBot &source)
+    {        
+        _image = new wxBitmap(*source._image); 
+        
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;        
+        _chatLogic = source._chatLogic;
+        std::cout << "Chatbot Copy Constructor" << std::endl;
+    }
+    // copy assignment operator override
+    ChatBot &operator=(const ChatBot &source)
+    {
+        if (this == &source)
+        {
+            return *this;
+        }
+        delete _image;
+        *_image = *source._image;
 
-    //// STUDENT CODE
-    ////
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+        std::cout << "Chatbot Copy Assignment Operator" << std::endl;
+    
+    }
+    
+    // move constructor
+    ChatBot (ChatBot &&source)
+    {
+        _image = source._image;
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
 
-    ////
-    //// EOF STUDENT CODE
+        source._image = NULL;
+        source._currentNode = nullptr;
+        source._rootNode = nullptr;
+        source._chatLogic = nullptr;
+        std::cout << "Chatbot Move Constructor" << std::endl;
+    }
+
+    // move assignment Operator overide
+    ChatBot &operator=(ChatBot &&source)
+    {
+        if(this == &source)
+        {
+            return *this;
+        }
+        delete _image;
+        _image = source._image;
+        _currentNode = source._currentNode;
+        _rootNode = source._rootNode;
+        _chatLogic = source._chatLogic;
+
+        source._image = NULL;
+        source._currentNode = nullptr;
+        source._rootNode = nullptr;
+        source._chatLogic = nullptr;
+        std::cout << "Chatbot Move Assignment Operator" << std::endl;
+    }
 
     // getters / setters
     void SetCurrentNode(GraphNode *node);
